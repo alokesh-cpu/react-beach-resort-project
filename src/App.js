d6396901cec93eb4e4875ebc0172dc1220f9587b
alokesh_cpu <bordoloialokesh999@gmail.com>
2020-07-10 23:30:35 +0530

    Beach resors project intial version

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+import NavBar from './components/Navbar';
+
+import {Route , Switch } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/rooms" component={Rooms}></Route>
+        <Route exact path="/rooms/:slug" component={SingleRoom}></Route>
+        <Route component={Error}></Route>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
